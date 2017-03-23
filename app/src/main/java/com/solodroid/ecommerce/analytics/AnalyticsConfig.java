f474ee48e3556683795aaf77999074b176fc7e70
peepaz <peepaz@gmail.com>
2017-03-23 04:16:22 -0400

    minor asthetics changes

--- app/src/main/java/com/solodroid/ecommerce/analytics/AnalyticsConfig.java
+++
@@ -1,8 +0,0 @@
-package com.solodroid.ecommerce.analytics;
-
-public class AnalyticsConfig {
-
-    // true for enabling Google Analytics, should be true in production release
-    public static final boolean ANALYTICS = true;
-
-}
